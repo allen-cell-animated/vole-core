c48b89f917f4fd6c997917fa58f85d552687de9f
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-04-20 10:34:15 -0700

    move parentheses around

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -55,7 +55,7 @@ function HSVtoRGB(h: number | HSVColor, s: number, v: number): [number, number, 
 // 1993 Park-Miller LCG
 function LCG(s: number) {
   return function () {
-    s = Math.imul(48271, s) | (0 % 2147483647);
+    s = (Math.imul(48271, s) | 0) % 2147483647;
     return (s & 2147483647) / 2147483648;
   };
 }
