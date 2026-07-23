a81406378dabbc533905d20e1e17bf166a9d0674
Philip Garrison <philip.garrison@alleninstitute.org>
2026-07-23 15:58:37 -0700

    Replace vite-plugin-glsl with inline plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import type { UserConfig } from "vite";
 // We want to be able to import glsl shaders without url decorations like ?raw.
 // This is because we still build the distribution with babel/tsc and don't want
 // to use nonstandard import syntax there.
-import glsl from "vite-plugin-glsl";
-
 export default {
-  plugins: [glsl()],
+  plugins: [
+    {
+      name: "raw-shaders",
+      // For glsl file extensions, resolve `import foo from "./bar.frag"` to the plain text source code of bar.frag
+      // JSON.stringify is required to escape special characters: backticks in `code` would otherwise be interpreted
+      // as Javascript.
+      transform: (code, id) =>
+        /\.(glsl|frag|vert|fs|vs)$/.test(id) ? `export default ${JSON.stringify(code)};` : undefined,
+    },
+  ],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
   },
