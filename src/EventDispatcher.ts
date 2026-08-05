d08d610c3d25ada5ae4a137c454ec4e471634696
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-08-05 12:38:24 -0700

    better generic type names and docs

--- src/EventDispatcher.ts
+++ src/EventDispatcher.ts
@@ -1,10 +1,14 @@
-export type Event<M extends Record<string, object>, E extends keyof M> = M[E] & { type: E };
-export type Listener<M extends Record<string, object>, E extends keyof M> = (event: Event<M, E>) => void;
+// An event dispatcher's *event map* maps event type names to event payloads.
+type EventMap = Record<string, object>;
+// An event is an object containing the event's payload plus its type name.
+export type Event<M extends EventMap, E extends keyof M> = M[E] & { type: E };
+// Listeners are functions that receive events.
+export type Listener<M extends EventMap, E extends keyof M> = (event: Event<M, E>) => void;
 
-export default class EventDispatcher<EventMap extends Record<string, object>> {
-  private listeners: { [E in keyof EventMap]?: Listener<EventMap, E>[] } = {};
+export default class EventDispatcher<M extends EventMap> {
+  private listeners: { [E in keyof M]?: Listener<M, E>[] } = {};
 
-  public addEventListener<E extends keyof EventMap>(event: E, listener: Listener<EventMap, E>): void {
+  public addEventListener<E extends keyof M>(event: E, listener: Listener<M, E>): void {
     if (this.listeners[event] === undefined) {
       this.listeners[event] = [];
     }
@@ -13,7 +17,7 @@ export default class EventDispatcher<EventMap extends Record<string, object>> {
     }
   }
 
-  public removeEventListener<E extends keyof EventMap>(event: E, listener: Listener<EventMap, E>): void {
+  public removeEventListener<E extends keyof M>(event: E, listener: Listener<M, E>): void {
     const listeners = this.listeners[event];
     if (listeners !== undefined) {
       const index = listeners.indexOf(listener);
@@ -23,7 +27,7 @@ export default class EventDispatcher<EventMap extends Record<string, object>> {
     }
   }
 
-  protected dispatchEvent<E extends keyof EventMap>(event: Event<EventMap, E>): void {
+  protected dispatchEvent<E extends keyof M>(event: Event<M, E>): void {
     const listeners = this.listeners[event.type];
     if (listeners !== undefined) {
       for (const listener of listeners) {
